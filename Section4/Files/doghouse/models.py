da35432ae553974fc6c6278e6720a86328dd5be8
Jacob Eriksson <jacob.eriksson@datadoghq.com>
2024-10-01 16:07:45 +0200

    update models

--- Section4/Files/doghouse/models.py
+++ Section4/Files/doghouse/models.py
@@ -1,7 +1,7 @@
 products = [
-    {'id': 1, 'name': 'Doghouse Classic', 'price': 49.99, 'description': 'A cozy place for your dog to rest.'},
-    {'id': 2, 'name': 'Deluxe Doghouse', 'price': 89.99, 'description': 'A luxurious doghouse with extra space.'},
-    {'id': 3, 'name': 'Portable Doghouse', 'price': 39.99, 'description': 'A lightweight doghouse perfect for travel.'},
+    {'id': 1, 'name': 'Doghouse Classic', 'price': 49.99, 'description': 'A cozy place for your dog to rest.', 'image': 'classic_doghouse.png'},
+    {'id': 2, 'name': 'Deluxe Doghouse', 'price': 89.99, 'description': 'A luxurious doghouse with extra space.', 'image': 'deluxe_doghouse.png'},
+    {'id': 3, 'name': 'Portable Doghouse', 'price': 39.99, 'description': 'A lightweight doghouse perfect for travel.', 'image': 'portable_doghouse.png'},
 ]
 
 # Climate data based on common cities or regions
